3a39a7ab3293aa7244dac7cc31be0ebc12a4d510
Jens de Rond <jensderond@gmail.com>
2019-08-17 12:07:36 +0200

    [TASK] Add basic files

--- api/main.go
+++ api/main.go
@@ -1,3 +1,14 @@
-package api
+package main
 
-// TODO: Implement app logic
+import (
+	"github.com/EdinTC/Checq/api/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/lookup-ip/:name", utils.QueryIPAdress)
+	router.GET("/lookup-txt/:name", utils.QueryTXT)
+	router.GET("/lookup-ns/:name", utils.QueryNS)
+	router.Run(":5000")
+}
