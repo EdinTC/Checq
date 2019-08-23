5889f003f9c2b58c2328c7d232f70191e8e40291
EdinTC <edintur@hotmail.nl>
2019-08-23 14:49:41 +0200

    [TASK] Changed API port to 1337

--- api/main.go
+++ api/main.go
@@ -10,5 +10,5 @@ func main() {
 	router.GET("/lookup-ip/:name", utils.QueryIPAdress)
 	router.GET("/lookup-txt/:name", utils.QueryTXT)
 	router.GET("/lookup-ns/:name", utils.QueryNS)
-	router.Run(":5000")
+	router.Run(":1337")
 }
